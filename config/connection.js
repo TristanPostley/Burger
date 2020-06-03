e3c2ba1029fd052e3966ab392ef1dead88420e75
Tristan Postley <tpostley@gmail.com>
2020-06-02 17:06:32 -0700

    Burgers

---
+++ config/connection.js
@@ -0,0 +1,32 @@
+// require("dotenv").config();
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "cl0cktower",
+    database: "burger_db"
+    // host: "localhost",
+    // user: process.env.MYSQL_USER,
+    // password: process.env.MYSQL_PASS,
+    // database: process.env.MYSQL_DB
+
+  });
+}
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
